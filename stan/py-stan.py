5fbdb7ef72b85d73c970885fe7053840190275d2
sstoltze <sstoltze@gmail.com>
2018-10-24 19:42:12 +0200

    More attempts to get graphics in pystan. Still not working.

--- stan/py-stan.py
+++ stan/py-stan.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import arviz as az # ??? matplotlib replacement?
+import pickle
 
 np.random.seed(33)
 
@@ -10,10 +11,17 @@ N = 50
 x = np.random.randn(N)
 
 model="model.stan"
-stan_model = pystan.StanModel(file=model)
+try:
+    with open("pickled_model.pickle", "rb") as pickle_file:
+        stan_model = pickle.load(pickle_file)
+except FileNotFoundError:
+    stan_model = pystan.StanModel(file=model)
+    with open("pickled_model.pickle", "wb") as pickle_file:
+        pickle.dump(stan_model, pickle_file)
 
 fit = stan_model.sampling(data={'N': N, 'x' : x}, chains=1)
 
 print(fit)
 
-az.plot_density(fit, var_names=['mu', 'sigma'])
+#fit.plot()
+#az.plot_density(fit, var_names=['mu', 'sigma'])
