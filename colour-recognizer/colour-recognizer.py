5af9f9a96a23f63c0d50ccd03054411eeb981782
sstoltze <sstoltze@gmail.com>
2018-11-13 16:42:59 +0100

    Small rework of colour-recognizer.py

--- colour-recognizer/colour-recognizer.py
+++ colour-recognizer/colour-recognizer.py
@@ -1,12 +1,10 @@
 # Based on https://www.tensorflow.org/tutorials/keras/basic_classification
+# Consider looking at https://medium.com/@nickbortolotti/what-is-your-favorite-tie-color-tensorflow-analysis-95c5f7554baa
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-print("TensorFlow:", tf.VERSION)
-print("Keras:", keras.__version__)
-print("Numpy:", np.version.full_version)
-
+image_size = 56
 class_names = ["Black", "Blue", "Green", "Orange",
                "Red", "Violet", "White", "Brown", "Yellow"]
 
@@ -15,7 +13,8 @@ class_names = ["Black", "Blue", "Green", "Orange",
 def image_parse_function(filename):
     image_string = tf.read_file(filename)
     image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    image_resized = tf.image.resize_images(image_decoded, [28, 28])
+    image_resized = tf.image.resize_images(image_decoded,
+                                           [image_size, image_size])
     return image_resized
 
 
@@ -65,26 +64,35 @@ tutorial_model = keras.Sequential([
     keras.layers.Dense(len(class_names), activation=tf.nn.softmax)
 ])
 
-# Let's try to optimize the model
-model = keras.Sequential([
-    # Make a 1d array instead of 2d
-    keras.layers.Flatten(input_shape=(28, 28, 3)),
-    # 128 neuron layer
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    # Softmax returns a probablility for each option
-    keras.layers.Dense(len(class_names), activation=tf.nn.softmax)
-])
+# Try/catch to only train the model once while testing.
+# Can be removed later
+try:
+    model
+except NameError:
+    print("Compiling model.")
+    model = keras.Sequential([
+        # Make a 1d array instead of 2d
+        keras.layers.Flatten(input_shape=(image_size, image_size, 3)),
+        # Neuron layer
+        keras.layers.Dense(12*image_size, activation=tf.nn.relu),
+        # Neuron layer
+        keras.layers.Dense(12*image_size, activation=tf.nn.relu),
+        # Neuron layer
+        keras.layers.Dense(12*image_size, activation=tf.nn.relu),
+        # Softmax returns a probablility for each option
+        keras.layers.Dense(len(class_names), activation=tf.nn.softmax)
+    ])
+    # Optimizer is how to update the model based on the result
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  # Loss function for the model
+                  loss='sparse_categorical_crossentropy',
+                  # Uses correct / total predictions to measure success
+                  metrics=['accuracy'])
 
-# Optimizer is how to update the model based on the result
-model.compile(optimizer=tf.train.AdamOptimizer(),
-              # Loss function for the model
-              loss='sparse_categorical_crossentropy',
-              # Uses correct / total predictions to measure success
-              metrics=['accuracy'])
-
-# This step takes a while
-model.fit(train_images, train_labels, epochs=5, steps_per_epoch=30)
+    # This step takes a while
+    model.fit(train_images, train_labels, epochs=5, steps_per_epoch=30)
 
 # Test the accuracy
+print("Testing accuracy.")
 test_loss, test_acc = model.evaluate(test_images, test_labels, steps=30)
 print('Test accuracy:', test_acc)
